6d66bc9063d35ca19ae54c3e27323d0b6c6d3a50
Jeova BL <jeovab115@gmail.com>
2025-03-18 17:01:56 -0300

    initial project

---
+++ src/test/java/com/backEndMobile/backEndMobile/BackEndMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backEndMobile.backEndMobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
