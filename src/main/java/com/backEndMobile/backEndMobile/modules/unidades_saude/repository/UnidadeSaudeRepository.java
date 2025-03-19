197edc7238571875769457674954fcdad7e7fbab
Jeova BL <jeovab115@gmail.com>
2025-03-19 17:49:38 -0300

    feat: add UnidadeSaudeRepository interface for data access

---
+++ src/main/java/com/backEndMobile/backEndMobile/modules/unidades_saude/repository/UnidadeSaudeRepository.java
@@ -0,0 +1,10 @@
+package com.backEndMobile.backEndMobile.modules.unidades_saude.repository;
+
+import com.backEndMobile.backEndMobile.modules.unidades_saude.domain.UnidadesSaude;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UnidadeSaudeRepository extends JpaRepository<UnidadesSaude, Long> {
+
+}
