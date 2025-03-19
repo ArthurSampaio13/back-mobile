77fb5bc2de74be4a9930b480b9369b387cb0f060
Jeova BL <jeovab115@gmail.com>
2025-03-19 17:50:29 -0300

    feat: add custom exception handling for blank values and invalid time ranges

---
+++ src/main/java/com/backEndMobile/backEndMobile/exceptions/BlankValueException.java
@@ -0,0 +1,11 @@
+package com.backEndMobile.backEndMobile.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BlankValueException extends RuntimeException {
+    public BlankValueException(String message) {
+        super(message);
+    }
+}
