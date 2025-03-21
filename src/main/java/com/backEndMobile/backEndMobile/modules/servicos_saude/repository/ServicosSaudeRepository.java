4abe26a93267290f9bfd5c3e122176bf4499bbc7
Jeova BL <jeovab115@gmail.com>
2025-03-21 17:15:33 -0300

    feat: add findByUnidadeSaudeId method to ServicosSaudeRepository for querying by health unit ID

--- src/main/java/com/backEndMobile/backEndMobile/modules/servicos_saude/repository/ServicosSaudeRepository.java
+++ src/main/java/com/backEndMobile/backEndMobile/modules/servicos_saude/repository/ServicosSaudeRepository.java
@@ -4,7 +4,11 @@ import com.backEndMobile.backEndMobile.modules.servicos_saude.domain.ServicosSau
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ServicosSaudeRepository extends JpaRepository<ServicosSaude, Long> {
     void deleteByUnidadeSaudeId(Long id);
+
+    List<ServicosSaude> findByUnidadeSaudeId(Long id);
 }
--- src/main/java/com/backEndMobile/backEndMobile/modules/unidades_saude/repository/UnidadeSaudeRepository.java
+++ src/main/java/com/backEndMobile/backEndMobile/modules/unidades_saude/repository/UnidadeSaudeRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UnidadeSaudeRepository extends JpaRepository<UnidadesSaude, Long> {
 
+
 }
