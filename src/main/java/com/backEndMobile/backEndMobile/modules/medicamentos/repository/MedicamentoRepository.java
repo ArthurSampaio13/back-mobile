c3cc80abd80bae7c4d242781cbf15a52ac602163
Jeova BL <jeovab115@gmail.com>
2025-03-22 18:30:24 -0300

    feat: add method to MedicamentoRepository for finding medications by health unit ID

--- src/main/java/com/backEndMobile/backEndMobile/modules/medicamentos/repository/MedicamentoRepository.java
+++ src/main/java/com/backEndMobile/backEndMobile/modules/medicamentos/repository/MedicamentoRepository.java
@@ -4,7 +4,9 @@ import com.backEndMobile.backEndMobile.modules.medicamentos.domain.Medicamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MedicamentoRepository extends JpaRepository<Medicamento, Long> {
-
+    List<Medicamento> findByUnidadesSaudeId(Long id);
 }
